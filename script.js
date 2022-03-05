088c31cea82b9486ae2ad2bd9c5c332f75dd726a
John <dab.cena420@gmail.com>
2022-03-04 16:18:31 -0800

    Created a grid of 16x16 using grid and template columns

--- script.js
+++ script.js
@@ -3,15 +3,31 @@
 var container = document.createElement("div");
 container.setAttribute('id', 'container');
 container.style.display = 'grid';
-container.style.gridTemplateColumns = 'auto auto auto';
-container.style.padding = '10px';
+container.style.gridTemplateColumns = 'repeat(16,100px)';
+//container.style.padding = '10px';
 
 document.body.appendChild(container);
 
 var box = document.createElement("div");
 box.classList.add('box');
-box.style.backgroundColor = 'black';
-box.style.border = '1px solid rgba(0,0,0,0)'; 
-box.style.padding = '40px';
 
-document.getElementById("container").appendChild(box);
+
+var test = document.createElement("div");
+test.classList.add('box');
+
+function createRow() { //adds 16 divs
+    for (let i=0; i < 16; i++) {
+        let temp = document.createElement("div");
+        temp.classList.add('box');
+        document.getElementById("container").appendChild(temp);
+        console.log("This is iteration " + i);
+    }
+}
+
+for (i=0; i<16; i++) { //lol
+    createRow(); 
+}
+
+
+//document.getElementById("container").appendChild(box);
+//document.getElementById("container").appendChild(test);
