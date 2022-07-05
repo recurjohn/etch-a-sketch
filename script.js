0c7553f6b4b094736d85085434c102881a3ae455
John D <dab.cena420@gmail.com>
2022-07-04 22:06:44 -0700

    Major overhaul, redid method

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
-//To make a 16x16 grid of divs, you probably need a div to hold all 16
+const grid = document.querySelector('.grid');
 
-var container = document.createElement("div");
-container.setAttribute('id', 'container');
-container.style.display = 'grid';
-container.style.gridTemplateColumns = 'repeat(16,100px)';
-//container.style.padding = '10px';
-
-document.body.appendChild(container);
-
-var box = document.createElement("div");
-box.classList.add('box');
-
-
-var test = document.createElement("div");
-test.classList.add('box');
-
-function createRow() { //adds 16 divs
-    for (let i=0; i < 16; i++) {
-        let temp = document.createElement("div");
-        temp.classList.add('box');
-        document.getElementById("container").appendChild(temp);
-        console.log("This is iteration " + i);
+function createGrid() {
+    for (let i = 0; i < 256; i++) {
+        const div = document.createElement('div'); //create div
+        div.classList.add('cell');
+        div.addEventListener('mouseover', function(event){ //change background on hover
+            event.target.style.backgroundColor = '#4B4B4B';
+        })
+        grid.appendChild(div);
     }
-}
+};
 
-for (i=0; i<16; i++) { //lol
-    createRow(); 
-}
-
-
-//document.getElementById("container").appendChild(box);
-//document.getElementById("container").appendChild(test);
+createGrid();
